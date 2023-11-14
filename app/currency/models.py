792da533e1a1930fe69f0fb1dd8692e67bb78bc6
wisp_mac <mail.for.wisp@gmail.com>
2023-11-14 22:09:15 +0200

    HW_5(Django models)

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Rate(models.Model):
+    buy = models.DecimalField(max_digits=6, decimal_places=2)
+    sell = models.DecimalField(max_digits=6, decimal_places=2)
+    created = models.DateTimeField()
+    type = models.CharField(max_length=3)
+    source = models.CharField(max_length=255)
+
+class ContactUs(models.Model):
+    email_from = models.EmailField(max_length=30)
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+
+
+
--- app/currency/views.py
+++ app/currency/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from currency.models import Rate, ContactUs
+def rate_list(request):
 
-# Create your views here.
+    results = []
+    rates = Rate.objects.all()
+
+    for rate in rates:
+        results.append(
+            f'ID: {rate.id}, buy: {rate.buy}, sell: {rate.sell}, type: {rate.type}, source: {rate.source},'
+            f'created: {rate.created} <br>'
+        )
+    return HttpResponse(str(results))
+
+def message_list(request):
+
+    results2 = []
+    contacts = ContactUs.objects.all()
+
+    for contact in contacts:
+        results2.append(
+            f'email: {contact.email_from}, subject: {contact.subject}, message: {contact.message} <br>'
+        )
+    return HttpResponse(str(results2))

d65a665a05fb146e8fa56c07dea2e6e5b5f2dc3c
wisp_mac <mail.for.wisp@gmail.com>
2023-12-18 18:11:09 +0200

    HW_12 (with corrections UserPassesTestMixin )

--- app/account/urls.py
+++ app/account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 
 from account.views import ProfileView, UserSignUpView, UserActivateView
 
--- app/currency/views.py
+++ app/currency/views.py
@@ -2,7 +2,7 @@
 
 # from django.contrib.auth.models import User
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
 from currency.models import Rate, ContactUs, Source
 from django.core.mail import send_mail
 from django.urls import reverse_lazy
